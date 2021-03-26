86454848d0f1aa18006f2e2c1100f385d11ddfde
AboAuther <1256446353@qq.com>
2021-03-26 16:39:04 +0800

    简易日志库

--- logger/mylogger/mylogger.go
+++ logger/mylogger/mylogger.go
@@ -1,3 +1,54 @@
 package mylogger
 
+import (
+	"errors"
+	"fmt"
+	"path"
+	"runtime"
+	"strings"
+)
+
 //自定义一个日志库
+type LogLevel uint16
+
+const (
+	UNKNOWN LogLevel = iota
+	DEBUG
+	TRACE
+	INFO
+	WARNING
+	ERROR
+	FATAL
+)
+
+func parseLogLevel(s string) (LogLevel, error) {
+	s = strings.ToLower(s)
+	switch s {
+	case "debug":
+		return DEBUG, nil
+	case "trace":
+		return TRACE, nil
+	case "info":
+		return INFO, nil
+	case "warning":
+		return WARNING, nil
+	case "error":
+		return ERROR, nil
+	case "fatal":
+		return FATAL, nil
+	default:
+		err := errors.New("未知日志级别")
+		return UNKNOWN, err
+	}
+}
+
+func getInfo(skip int) (funcName, fileName string, lineNo int) {
+	pc, file, lineNo, ok := runtime.Caller(skip)
+	if !ok {
+		fmt.Printf("runtime.Caller() failed\n")
+		return
+	}
+	funcName = runtime.FuncForPC(pc).Name()
+	fileName = path.Base(file)
+	return
+}
